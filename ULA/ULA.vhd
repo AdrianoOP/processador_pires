c2feb273b9d0dab370bea8aafb780f5bc77e9510
AdrianoOP <oliveira.pires@gmail.com>
2013-08-23 18:27:49 -0300

    commit dos arquivos modificados com as atualizacoes da ULA

--- ULA/ULA.vhd
+++ ULA/ULA.vhd
@@ -1,18 +1,19 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.All;
 USE IEEE.std_logic_arith.All;
-USE IEEE.math_real.All;
+USE IEEE.std_logic_unsigned.All;
 
 -- ---comandos da ULA:--- ----
 --	PASSA_C, 0000
 --	PASSA_D, 0001
---	AND 		0010
---	OR 		0011
---	NOT 		0100
---	ADD 		0101
+--	C AND D	0010
+--	C OR	D	0011
+--	NOT C		0100
+--	C+D 		0101
 --	INC_C 	0110
---	MUL_LSB	0111
---	MUL_HSB	1000
+--	LSB(CxD)	0111
+--	HSB(CxD)	1000
+--	C-D		1001
 
 ENTITY ULA IS
 	PORT(	C,D		:	IN STD_LOGIC_VECTOR (7 DOWNTO 0);
@@ -23,7 +24,8 @@ ENTITY ULA IS
 END ENTITY ULA;
 
 ARCHITECTURE ula_descrita OF ULA IS
-	SIGNAL temp_int	:	INTEGER RANGE -128 TO 128;
+	SIGNAL temp_mult	:	STD_LOGIC_VECTOR (15 DOWNTO 0);
+	SIGNAL temp			: 	STD_LOGIC_VECTOR (7 DOWNTO 0);
 BEGIN
 	processo_ULA: PROCESS( ULA_ctrl, C, D ) -- A ULA eh sensivel a ULA_ctrl, C e D 
 	BEGIN
@@ -34,18 +36,43 @@ BEGIN
 			WHEN "0001" => -- equivale ao PASSA_D
 				Z<='0';N<='0';
 				saida<=D;
-			WHEN "0010" => -- equivale ao AND
+			WHEN "0010" => -- equivale ao C AND D
 				Z<='0';N<='0';
 				saida<=C AND D;
-			WHEN "0011" => -- equivale ao OR
+			WHEN "0011" => -- equivale ao C OR D
 				Z<='0';N<='0';
 				saida<=C OR D;
-			WHEN "0100" => -- equivale ao NOT
+			WHEN "0100" => -- equivale ao NOT C
 				Z<='0';N<='0';
 				saida<=NOT C;
-			WHEN "0101" => -- equivale ao ADD
+			WHEN "0101" => -- equivale ao C+D
 				Z<='0';N<='0';
-				--saida<=C+D; -- overfflow eh ignorado
+				saida<=C+D; -- overfflow eh ignorado
+			WHEN "0110" => -- equivale ao INC_C
+				Z<='0'; N<='0';
+				saida<=C+1;
+			WHEN "0111" => -- equivale ao MUL_LSB
+				Z<='0';N<='0';
+				temp_mult<=C*D; -- sinal intermediario para guardar a multiplicacao
+				saida<= temp_mult(7 DOWNTO 0);
+			WHEN "1000"	=>	--	equivale ao MUL_HSB
+				Z<='0';N<='0';
+				temp_mult<=C*D; -- sinal intermediario para guardar a multiplicacao
+				saida<= temp_mult(15 DOWNTO 8);
+			WHEN "1001"	=>	--	equivale ao C-D
+				temp<=C-D;
+				IF temp<0 THEN
+					N<='1';
+				ELSE
+					N<='0';
+				END IF;
+				
+				IF temp=0 THEN
+					Z<='1';
+				ELSE
+					Z<='0';
+				END IF;
+				saida<=temp;
 			WHEN OTHERS =>
 				-- nada acontece
 				-- eh melhor que eu coloque um tristate ('Z') na saida?
